f5ad9b634e4e4968ed85e66ced9953c8dd0582fd
Joel <joel.developpeur@gmail.com>
2020-12-19 21:27:20 +0000

    Maj de la db - ajout de location à ind_announces

---
+++ db.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: inoveinn_wp806
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ind_announces`
+--
+
+DROP TABLE IF EXISTS `ind_announces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ind_announces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `id_sub_category` int(11) DEFAULT NULL,
+  `user_email_address` varchar(255) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `state` int(11) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `posted_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `views` int(11) DEFAULT '0',
+  `icon_class` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_email_address` (`user_email_address`),
+  KEY `fk_id_category` (`id_category`),
+  KEY `fk_id_sub_category` (`id_sub_category`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ind_announces`
+--
+
+LOCK TABLES `ind_announces` WRITE;
+/*!40000 ALTER TABLE `ind_announces` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ind_announces` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ind_categories`
+--
+
+DROP TABLE IF EXISTS `ind_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ind_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `description` text,
+  `icon_class` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_title` (`title`),
+  UNIQUE KEY `uni_slug` (`slug`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ind_categories`
+--
+
+LOCK TABLES `ind_categories` WRITE;
+/*!40000 ALTER TABLE `ind_categories` DISABLE KEYS */;
+INSERT INTO `ind_categories` VALUES (1,'Jeu de football','jeu-de-football','2020-11-27 23:54:37',NULL,'C\'est un jeu de football',NULL);
+/*!40000 ALTER TABLE `ind_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ind_sub_categories`
+--
+
+DROP TABLE IF EXISTS `ind_sub_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ind_sub_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` datetime DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_title` (`title`) USING BTREE,
+  UNIQUE KEY `uni_slug` (`slug`),
+  KEY `fk_id_category` (`id_category`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ind_sub_categories`
+--
+
+LOCK TABLES `ind_sub_categories` WRITE;
+/*!40000 ALTER TABLE `ind_sub_categories` DISABLE KEYS */;
+INSERT INTO `ind_sub_categories` VALUES (1,'Jeu de football','jeu-de-football',1,'2020-11-27 23:57:24',NULL,'C\'est un jeu de football');
+/*!40000 ALTER TABLE `ind_sub_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ind_users`
+--
+
+DROP TABLE IF EXISTS `ind_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ind_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `first_names` varchar(255) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `type` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_pseudo` (`pseudo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ind_users`
+--
+
+LOCK TABLES `ind_users` WRITE;
+/*!40000 ALTER TABLE `ind_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ind_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-19 21:26:22
