44337abdd397ff7d8dc2d0f552f402625ecb0863
cartercurtis <75700138+cartercurtis@users.noreply.github.com>
2020-12-22 21:08:14 +0000

    merge

--- src/View/Page/Footer.php
+++ src/View/Page/Footer.php
@@ -37,115 +37,6 @@ class Footer extends Snippet
         return <<<HTML
         <!-- Footer Section Start -->
         <footer>
-<<<<<<< HEAD
-        <!-- Footer Area Start -->
-        <section class="footer-Content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
-                        <div class="widget">
-                            <h3 class="footer-logo"><img src="assets/img/logo/logo1.png" alt=""></h3>
-                            <div class="textwidget">
-
-                                <p>Indice est un site de petites annonces reputée pour être l'une des meilleurs en côte d'Ivoire , Indice vous offre la Possibilité de poster sans contrainte vos annonces sans prise de tête.</p>
-                                <p>Petit texte descrioptif, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis tincidunt est, et euismod purus suscipit quis. Etiam euismod ornare elementum. Sed ex est, consectetur eget facilisis sed, auctor ut purus.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
-                        <div class="widget">
-                            <h3 class="block-title">Derniers articles ajoutés</h3>
-                            <ul class="media-content-list">
-                                <li>
-                                    <div class="media-left">
-                                        <img class="img-fluid" src="assets/img/art/img1.jpg" alt="">
-                                        <div class="overlay">
-                                            <span class="price">$ 79</span>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        <h4 class="post-title"><a href="ads-details.html">Brand New Macbook Pro</a></h4>
-                                        <span class="date">12 Jan 2018</span>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="media-left">
-                                        <img class="img-fluid" src="assets/img/art/img2.jpg" alt="">
-                                        <div class="overlay">
-                                            <span class="price">$ 49</span>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        <h4 class="post-title"><a href="ads-details.html">Canon Photography Camera</a></h4>
-                                        <span class="date">28 Mar 2018</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
-                        <div class="widget">
-<<<<<<< HEAD
-                            <h3 class="block-title">Aide et Support</h3>
-                            <ul class="menu">
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">Nous Contacter</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
-                        <div class="widget">
-                            <h3 class="block-title">s'Inscrire</h3>
-                            <p class="text-sub">Site d'annonce numero 1 en Côte d'Ivoire</p>
-                            <h3 class="block-title">Inscrivez-vous</h3>
-
-                            <form method="post" id="subscribe-form" name="subscribe-form" class="validate">
-                                <div class="form-group is-empty">
-                                    <input type="email" value="" name="Email" class="form-control" id="EMAIL" placeholder="Adresse" required="">
-                                    <button type="submit" name="subscribe" id="subscribes" class="btn btn-common sub-btn"><i class="lni-check-box"></i></button>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </form>
-                            <ul class="footer-social">
-                                <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
-                                <li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
-                                <li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
-                                <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Footer area End -->
-
-        <!-- Copyright Start  -->
-        <div id="copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="site-info float-left">
-<<<<<<< HEAD
-                            <p>Tout droit reservé &copy; 2018 - Designé par <a href="https://indice.com" rel="nofollow">Indice</a></p>
-                        </div>
-                        <div class="float-right">
-                            <ul class="bottom-card">
-                                <li>
-                                    <a href="#"><img src="assets/img/footer/card1.jpg" alt="card"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="assets/img/footer/card2.jpg" alt="card"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="assets/img/footer/card3.jpg" alt="card"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="assets/img/footer/card4.jpg" alt="card"></a>
-                                </li>
-                            </ul>
-                            <p>Tous droits réservés &copy; 2020.</p>
-
-=======
             <!-- Footer Area Start -->
             <section class="footer-Content">
                 <div class="container">
@@ -195,7 +86,6 @@ class Footer extends Snippet
                             <div class="site-info float-left">
                                 <p>Tous droits réservés &copy; 2020.</p>
                             </div>
->>>>>>> 3550c19236fd6ebf21a2210d17b4144ebf72b7ee
                         </div>
                     </div>
                 </div>
